8d8989c114d671dbfc9a6ca831d53beb23e330ad
Zay <121255686+Benjaminguillermoburgos@users.noreply.github.com>
2026-06-26 18:52:15 -0400

    Fix Paciente activar ruta

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -4,31 +4,42 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-// Importaciones
+// Importaciones de Repositorios
 import com.example.repository.PersonalRepository
-import com.example.repository.MedicalRecordRepository // <-- Nueva
-import com.example.services.MedicalService           // <-- Nueva
+import com.example.repository.MedicalRecordRepository
+import com.example.repository.PatientRepository // <-- AGREGADO
+
+// Importaciones de Servicios
+import com.example.services.MedicalService
 import com.example.services.PersonalService
+
+// Importaciones de Rutas
 import com.example.routes.userRoutes
 import com.example.routes.medicalRoutes
+import com.example.routes.patientRoutes // <-- AGREGADO
 
 fun Application.configureRouting() {
-    // 1. Instanciamos las capas de USUARIOS
+    // 1. Instanciamos las capas de PERSONAL
     val personalRepository = PersonalRepository()
     val personalService = PersonalService(personalRepository)
 
-    // 2. Instanciamos las capas de MÉDICA
+    // 2. Instanciamos las capas MÉDICAS
     val medicalRepository = MedicalRecordRepository()
-    // Nota: El servicio médico necesita el repositorio médico Y el de usuarios (para buscar nombres de doctores)
     val medicalService = MedicalService(medicalRepository, personalRepository)
+
+    // 3. Instanciamos la capa de PACIENTES (Faltaba esto)
+    val patientRepository = PatientRepository()
 
     routing {
         get("/") {
             call.respondText("¡El servidor del Hospital está funcionando!")
         }
 
-        // Activamos las rutas
+        // Activamos TODAS las rutas
         userRoutes(personalService)
         medicalRoutes(medicalService)
+
+        // ¡LA PIEZA CLAVE QUE FALTABA!
+        patientRoutes(patientRepository)
     }
 }
