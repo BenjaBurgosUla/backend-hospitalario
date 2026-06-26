ff7944718cac4407ee1a277b9f2711ec1e6e58ad
Zay <121255686+Benjaminguillermoburgos@users.noreply.github.com>
2026-06-26 15:37:08 -0400

    a

--- src/main/kotlin/config/DatabaseFactory.kt
+++ src/main/kotlin/config/DatabaseFactory.kt
@@ -1,5 +1,6 @@
 package com.example.config
 
+import com.example.models.MedicalRecords
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
